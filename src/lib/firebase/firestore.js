21497fd115ef0cb3745447fd655e32d95093ee9b
Matthew <matteidsen@gmail.com>
2025-10-14 14:33:09 -0500

    allow users to submit restaurant reviews

--- src/lib/firebase/firestore.js
+++ src/lib/firebase/firestore.js
@@ -17,6 +17,7 @@ import {
 } from "firebase/firestore";
 
 import { db } from "@/src/lib/firebase/clientApp";
+import { ErrorBoundaryHandler } from "next/dist/client/components/error-boundary";
 
 export async function updateRestaurantImageReference(
   restaurantId,
@@ -34,11 +35,46 @@ const updateWithRating = async (
   newRatingDocument,
   review
 ) => {
-  return;
+  const restaurant = await transaction.get(docRef);
+  const data = restaurant.data();
+  const newNumRatings = data?.numRatings ? data.numRatings + 1 : 1;
+  const newSumRating = (data?.sumRating || 0) + Number(review.rating);
+  const newAverage = newSumRating / newNumRatings;
+
+  transaction.update(docRef, {
+    numRatings: newNumRatings,
+    sumRating: newSumRating,
+    avgRating: newAverage,
+  });
+
+  transaction.set(newRatingDocument, {
+    ...review,
+    timestamp: Timestamp.fromDate(new Date()),
+  });
 };
 
 export async function addReviewToRestaurant(db, restaurantId, review) {
-  return;
+  if (!restaurantId) {
+    throw new Error("No restaurant ID has been provided.");
+  }
+
+  if (!review) {
+    throw new Error("A valid review has not been provided");
+  }
+
+  try {
+    const docRef = doc(collection(db, "restaurants"), restaurantId);
+    const newRatingDocument = doc(
+      collection(db, `restaurants/${restaurantId}/ratings`)
+    );
+
+    await runTransaction(db, transaction =>
+      updateWithRating(transaction, docRef, newRatingDocument, review)
+    );
+  } catch (error) {
+    console.error("There was an error adding the rating to the restaurant", error);
+    throw error;
+  }
 }
 
 function applyQueryFilters(q, { category, city, price, sort }) {
