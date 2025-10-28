d44ba6d6bb01c6eaf8e67203074c8cc1ecb70482
Matthew <matteidsen@gmail.com>
2025-10-28 18:48:17 -0500

    fix actions.js not pulling currentUser from getAuthenticatedAppForUser

--- src/app/actions.js
+++ src/app/actions.js
@@ -8,7 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions
 // Replace the function below
 export async function handleReviewFormSubmission(data) {
-    const { app } = await getAuthenticatedAppForUser();
+    const { app, currentUser } = await getAuthenticatedAppForUser();
     const db = getFirestore(app);
 
     await addReviewToRestaurant(db, data.get("restaurantId"), {
